ac97277fb000a59e9daba34a593af5d655857e60
Roman Surinov <romasurinov13@mail.ru>
2021-03-23 22:51:38 +0300

    practice3

---
+++ 3practice/pep8.py
@@ -0,0 +1,30 @@
+from Dop_examples import part1; part1()
+
+with open ('file.dat') as f:
+    contents = f.read()
+
+age = 2
+if age>15:
+    print('Can drive')
+
+my_tuple = 1,2,3
+
+def func(key1 = 'val1',
+         key2 = 'val2'):
+    return key1, key2
+
+
+x = 1
+if x > 5: y = 10
+
+
+var = 1
+if var != True:
+    print("var is not equal to True")
+if var == None:
+    print("var is equal to None")
+
+
+x = True
+if x == True:
+    print('True!')
